768f03796249aaf798827a900d9f1513e50ac3a2
Rui Costa <rui.pfcosta@gmail.com>
2015-12-01 00:29:11 +0000

    Initial Commit

---
+++ AutoLayoutPlus/AutoLayoutPlus.h
@@ -0,0 +1,19 @@
+//
+//  AutoLayoutPlus.h
+//  AutoLayoutPlus
+//
+//  Created by Rui Costa on 01/12/15.
+//  Copyright © 2015 Rui Costa. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for AutoLayoutPlus.
+FOUNDATION_EXPORT double AutoLayoutPlusVersionNumber;
+
+//! Project version string for AutoLayoutPlus.
+FOUNDATION_EXPORT const unsigned char AutoLayoutPlusVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AutoLayoutPlus/PublicHeader.h>
+
+
