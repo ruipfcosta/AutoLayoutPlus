6586474cd86ffb821af9a2e0a7279d20e763c8ce
Rui Costa <rui.pfcosta@gmail.com>
2016-01-29 20:43:34 +0000

    Added tvOS target.

---
+++ AutoLayoutPlusTV/AutoLayoutPlusTV.h
@@ -0,0 +1,19 @@
+//
+//  AutoLayoutPlusTV.h
+//  AutoLayoutPlusTV
+//
+//  Created by Rui Costa on 29/01/2016.
+//  Copyright © 2016 Rui Costa. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for AutoLayoutPlusTV.
+FOUNDATION_EXPORT double AutoLayoutPlusTVVersionNumber;
+
+//! Project version string for AutoLayoutPlusTV.
+FOUNDATION_EXPORT const unsigned char AutoLayoutPlusTVVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AutoLayoutPlusTV/PublicHeader.h>
+
+
